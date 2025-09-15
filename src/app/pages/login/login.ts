fb3ac0d622f57e139132c1f18afc9f288111b433
Tudor-debug819 <tovidiua@emeal.nttdata.com>
2025-09-15 13:51:54 +0300

    add login reactive form group

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      email: [''],
+      password: [''],
+    });
+  }
+
+  submit() {
+    console.log('Login form value:', this.form.getRawValue());
+  }
 
 }
