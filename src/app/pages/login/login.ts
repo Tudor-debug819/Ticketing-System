c5c05bd40371f8c7b305a525d6ddafcc9d61f280
Tudor-debug819 <tovidiua@emeal.nttdata.com>
2025-09-16 14:19:23 +0300

    Feature 9/login page (#39)
    * finish install tailwind
    
    * create not found page
    
    * import routerlink
    
    * add login reactive form group
    
    * add validation for email and password
    
    * add angular material inports and UI states
    
    * finish login page

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule, CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+
+  hide = signal(true);
+  submitting = signal(false);
+
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  submit() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+    this.submitting.set(true);
+    console.log('Login form value:', this.form.getRawValue());
+    setTimeout(() =>
+      this.submitting.set(false), 400);
+  }
 
 }
