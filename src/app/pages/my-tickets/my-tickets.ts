488364fc91516b2a9143df93fe4ac309f8a859fa
Tudor-debug819 <tovidiua@emeal.nttdata.com>
2025-09-27 16:17:39 +0300

    finish my tickets page

--- src/app/pages/my-tickets/my-tickets.ts
+++ src/app/pages/my-tickets/my-tickets.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Ticket } from '../../ticket.model';
+import { TicketService } from '../../services/ticket.service';
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-my-tickets',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './my-tickets.html',
   styleUrl: './my-tickets.css'
 })
-export class MyTickets {
+export class MyTickets implements OnInit {
+
+  tickets$!: Observable<Ticket[]>;
+  constructor(private tickets: TicketService, private auth: AuthService) { }
+
+  ngOnInit(): void {
+    const me = this.auth.currentUser!;
+    this.tickets$ = this.tickets.getByReporter(me.id);
+  }
 
 }
