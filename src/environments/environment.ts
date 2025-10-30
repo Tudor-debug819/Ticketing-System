06e2382d06ed6972cf5bab5e57977790318ebd69
Tudor-debug819 <tovidiua@emeal.nttdata.com>
2025-10-30 14:36:40 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://ticketing-system-backend-czjl.onrender.com' // URL-ul backend-ului NestJS
+    apiUrl: 'https://ticketing-system-backend-czjlk.onrender.com' // URL-ul backend-ului NestJS
 };
